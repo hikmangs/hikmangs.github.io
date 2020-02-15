c3d5fd3e59ee9ff4659d616e964861485d09bfeb
Hikman Ganda Sasmita <hikman.lord.leader@gmail.com>
2020-02-15 16:04:22 +0700

    commit awal

---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simas Jabar</title>
+	 <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+			<link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<nav>
+				<div class="nav-wrapper">
+					<div class="col s12 center-align">
+						<h4>CURRICULUM VITAE</h4>
+					</div>
+				</div>
+			</nav>
+			<div class="col s9">
+				<div class="card-panel">
+					<span class="card-title">
+						<h4><i class="material-icons">account_box</i> Personal Information</h4>
+					</span>
+					<table class="bordered">
+				        <tbody>
+				          <tr>
+				            <td>Nama Lengkap</td>
+				            <td>: Hikman Ganda Sasmita</td>
+				          </tr>
+									<tr>
+				            <td>Tempat, Tanggal Lahir</td>
+				            <td>: Bogor, 25 April 1994 </td>
+				          </tr>
+									<tr>
+				            <td>Jenis Kelamin</td>
+				            <td>: Laki-laki</td>
+				          </tr>
+									<tr>
+				            <td>Agama</td>
+				            <td>: Islam</td>
+				          </tr>
+									<tr>
+				            <td>Gol. Darah</td>
+				            <td>: AB</td>
+				          </tr>
+									<tr>
+				            <td>Status</td>
+				            <td>: Belum Menikah</td>
+				          </tr>
+									<tr>
+				            <td>Tinggi/Berat Badan</td>
+				            <td>: 172 cm/65 Kg</td>
+				          </tr>
+				          <tr>
+				            <td>Alamat</td>
+				            <td>: Kp. Sudimampir Rt. 01 Rw. 02 Ds. Cimanggis Kec.Bojonggede Kab. Bogor Indonesia 16320</td>
+				          </tr>
+				 		</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="col s3">
+					<div class="card-panel hoverable">
+					 	<img class="responsive-img" src="images/foto.jpg">
+				</div>
+				 <blockquote>
+  				<b>No. Kontak</b><br>
+						(+62)85697696396
+			</blockquote>
+				<blockquote>
+					 <b>E-Mail</b><br>
+					 hikman.lord.leader@gmail.com
+			 </blockquote>
+			</div>
+			<div class="col s6">
+					<div class="card-panel">
+						<div class="card-title">
+						<h4><i class="material-icons">school</i> Riwayat Pendidikan</h4>
+						</div>
+					<table class="bordered">
+				        <tbody>
+					        <tr>
+					            <td width="70%">SDN Cimanggis 03 Bojonggede</td>
+					            <td width="30%">1999 – 2005</td>
+					        </tr>
+							<tr>
+					            <td width="70%">SMPN 1 Bojonggede</td>
+					            <td width="30%">2005 – 2008</td>
+					        </tr>
+					        <tr>
+					            <td width="70%">SMK Bina Warga 1 Bogor</td>
+					            <td width="30%">2008 – 2011</td>
+					        </tr>
+							<tr>
+					            <td width="70%">Universitas Komputer Indonesia (Sistem Komputer S1)</td>
+					            <td width="30%">2011 – 2017</td>
+					        </tr>
+				 		</tbody>
+					</table>
+					</div>
+				</div>
+				<div class="col s6">
+					<div class="card-panel">
+						<div class="card-title">
+						<h4><i class="material-icons">school</i> Pengalaman</h4>
+						</div>
+						<ul class="collection">
+					      	<li class="collection-item">Praktek Kerja Lapangan: PT. Indosat Tbk, Bogor (06 Oktober 2009 - 31 Desember 2009)</li>
+					      	<li class="collection-item">Organisasi : Asisten Di Lab. Sistem Digital Unikom (15 February 2015 - 09 January 2016)</li>
+					      	<li class="collection-item">Organisasi : Asisten Di Lab. Elektronika Unikom (15 February 2016 -  Mei 2017)</li>
+					     	<li class="collection-item">Organisasi : Ketua Riset (IoT) di Divisi Embedded Research Group(ERG) Unikom (15 February 2016 - Sekarang)</li>
+					    </ul>
+					</div>
+				</div>
+		</div>
+		<div class="row">
+			<div class="col s6">
+			<div class="card-panel">
+						<div class="card-title">
+						<h4><i class="material-icons">school</i> Kemampuan</h4>
+						</div>
+						<ul class="collection">
+					      	<li class="collection-item">
+					      		<div class="progress">
+								    <div class="determinate" style="width: 70%"></div>
+								</div>
+					      	</li>
+					      	<li class="collection-item">
+					      		<div class="progress">
+								    <div class="determinate" style="width: 70%"></div>
+								</div>
+					      	</li>
+					      	<li class="collection-item">
+					      		<div class="progress">
+								    <div class="determinate" style="width: 70%"></div>
+								</div>
+					      	</li>
+					     	<li class="collection-item">
+					     	 <div class="left">
+					     	 	0%
+					     	 </div>
+					     	 <div class="right">
+					     	 	100%
+					     	 </div>
+					     		<div class="progress">
+								   <div class="determinate" style="width: 70%">
+								   </div>
+								</div>
+
+					     	</li>
+					    </ul>
+					</div>
+				
+			</div>
+			<div class="col s6">
+				<div class="card-panel">
+						<div class="card-title">
+						<h4><i class="material-icons">school</i> Pengalaman</h4>
+						</div>
+						<ul class="collection">
+					      	<li class="collection-item"></li>
+					      	<li class="collection-item"></li>
+					      	<li class="collection-item"></li>
+					     	<li class="collection-item"></li>
+					    </ul>
+					</div>
+			</div>
+		</div>
+</div>
+<!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+
+</body>
+</html>
